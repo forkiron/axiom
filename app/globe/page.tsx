ac593c2a924438d95ddaa5c7f673e6ded5ec40e7
Thomas Lenh <thomaslenh@gmail.com>
2026-03-07 17:56:59 -0500

    fix: move analyzer overlay from landing to globe

--- app/globe/page.tsx
+++ app/globe/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { AnalyzerOverlay } from '../../components/analyzer/AnalyzerOverlay';
 import BackboardQuestionBox from '../../components/BackboardQuestionBox';
 import { WorldGlobeMap } from '../../components/globe/WorldGlobeMap';
 
@@ -7,6 +8,7 @@ export default function GlobePage() {
   return (
     <main className="h-screen w-screen overflow-hidden bg-black">
       <WorldGlobeMap className="h-screen min-h-screen w-screen" />
+      <AnalyzerOverlay />
       <BackboardQuestionBox />
     </main>
   );
